16c9650079f33ff1e69f2d0595e2cc4306a9f487
agencialumen <lumendigitalonline@gmail.com>
2025-06-30 15:14:04 -0300

    first commit

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -6,7 +6,7 @@ import { Instagram, Youtube, Music, Mail } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
 const heroImages = [
-  "/placeholder.svg?height=1080&width=1920",
+  "https://iili.io/F7C0CN9.md.jpg",
   "/placeholder.svg?height=1080&width=1920",
   "/placeholder.svg?height=1080&width=1920",
 ]
